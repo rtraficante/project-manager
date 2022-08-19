ea8cb0800241ae2a911ceb2ffbe76a3735b92b9d
RTraficante <rtraficante22@gmail.com>
2022-08-18 23:37:18 -0400

    more cookies

--- server/src/app.js
+++ server/src/app.js
@@ -38,7 +38,7 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "lax",
     },
     saveUninitialized: false,
