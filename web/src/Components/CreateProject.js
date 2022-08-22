b1bd135c7ab48bc5baf9215bc3d32ac7e8bdb48d
RTraficante <rtraficante22@gmail.com>
2022-08-22 16:43:02 -0400

    changed styling for create project form

--- web/src/Components/CreateProject.js
+++ web/src/Components/CreateProject.js
@@ -26,20 +26,23 @@ function CreateProject({ user, openCreateProject, setOpenCreateProject }) {
 
   return (
     <div className="absolute flex justify-center w-screen h-screen z-50 bg-gray-600 bg-opacity-60">
-      <div className="border mt-28 w-3/4 max-w-[500px] h-[90px] bg-white rounded-md drop-shadow">
-        <div>
+      <div className="border mt-28 w-3/4 max-w-[500px] p-2 h-[120px] bg-white rounded-md drop-shadow">
+        <div className="flex mt-1 my-4 items-center">
+          <h2 className="justify-self-center flex-1 text-center text-lg">
+            Create A New Project
+          </h2>
           <XIcon
-            className="w-5 right-0 top-0 m-2 absolute cursor-pointer hover:scale-110"
+            className="w-5 justify-self-end cursor-pointer hover:scale-110"
             onClick={() => setOpenCreateProject(false)}
           />
         </div>
         <form
-          className="p-6 flex space-x-4 justify-center"
+          className="flex space-x-4 justify-center px-2"
           onSubmit={handleSubmit}
         >
           <input
             ref={nameRef}
-            className="border p-2 rounded-md"
+            className="border p-2 rounded-md flex-1"
             placeholder="Project name..."
           />
           <button className="bg-blue-500 p-2 rounded-md text-white hover:bg-blue-700 active-bg-blue-800">
