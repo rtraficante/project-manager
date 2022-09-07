0c7472aac2fe21540cf8b7f208540bbdfcbccd3d
RTraficante <rtraficante22@gmail.com>
2022-09-07 18:09:07 -0400

    added empty message if no tasks available

--- web/src/Components/ProjectStatusContainer.js
+++ web/src/Components/ProjectStatusContainer.js
@@ -7,20 +7,26 @@ function ProjectStatusContainer({
   setShowTaskInfo,
   setTaskShown,
 }) {
+  const tasks = project.getProject.tasks.filter(
+    (task) => task.status === status
+  );
+
   return (
     <div className="bg-gray-200 shadow-md p-2 h-fit max-h-[85vh] w-[280px] rounded">
       <h4 className="text-center font-bold">{status}</h4>
       <div className="space-y-2 rounded-md mt-2 overflow-y-auto max-h-[55vh]">
-        {project.getProject.tasks
-          .filter((task) => task.status === status)
-          .map((task) => (
+        {tasks.length > 0 ? (
+          tasks.map((task) => (
             <Task
               key={task.id}
               setTaskShown={setTaskShown}
               setShowTaskInfo={setShowTaskInfo}
               task={task}
             />
-          ))}
+          ))
+        ) : (
+          <p className="text-sm text-center">No tasks in this section</p>
+        )}
       </div>
     </div>
   );
